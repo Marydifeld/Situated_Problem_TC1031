3f510b8fdb0ede0516aa72e1bc6eeecfb50942a0
marielletrevino16 <a01199250@tec.mx>
2025-09-11 12:51:36 -1200

    added class attibutes

--- Order.h
+++ Order.h
@@ -2,7 +2,24 @@
 #define ORDER_H
 class Order{
     private:
+      string date;
+      string restaurantName;
+      int hour;
+      int minute;
+      int second;
+      string orderItem;
+      int orderPrice;
     public: 
+      Order(string date, string restaurantName, int hour, int minute, int second, string orderItem, int orderPrice);
+      getDate();
+      getrestaurantName();
+      getHour();
+      getminute();
+      getsecond();
+      getorderItem();
+      getorderPrice();
+      sethour(int hour, int minute, int second);
+
 };
 
 #endif /*ORDER_H*/
