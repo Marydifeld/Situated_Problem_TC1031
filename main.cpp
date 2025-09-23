30362d68297253acbf01b47cf13966e4de9a3116
Marydifeld <a01739084@tec.mx>
2025-09-22 18:51:14 -0600

    Fixed date format

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
+#include<string>
+#include <stdio.h>
+#include <stdlib.h>
+#include<fstream>
+using namespace std; 
+void readFile(){
+    string sline; 
+    int index; 
+
+    ifstream inFile("orders.txt");
+    ofstream outFile("out2.txt");
+
+    //Check if it worked correctly
+    if (inFile.is_open() && outFile.is_open() ){
+        while ( getline(inFile, sline) ){
+            cout << "Read:\t"<< sline << endl;
+            outFile << sline << endl;
+        }
+    }
+    inFile.close();
+    outFile.close();
+}
 
 int main(){
-    
+    readFile();
     return 0; 
 }
+
