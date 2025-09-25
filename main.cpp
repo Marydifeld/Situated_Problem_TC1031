10bee909612ef42a06fe298e46d6ce153fbbca54
Marydifeld <a01739084@tec.mx>
2025-09-25 12:02:27 -0600

    Made search inclusive of end date

--- main.cpp
+++ main.cpp
@@ -19,12 +19,17 @@ int main(){
     string start_date, end_date; 
     cout << "Wellcome to the search tool by date" << endl;
     cout << "Please be mindfull of the requested format. This tool currently doesn't support years. (dd/mm)" << endl;
-    cout << "The end date is exclusive." << endl;
     cout << "Enter start and end dates separated by a blank space. (dd/mm dd/mm)" << endl; 
     cin >> start_date >> end_date; 
+    time_t start, end; 
+
+    start = inputToTimeT(stoi(start_date.substr(3, 2)) - 1, stoi(start_date.substr(0, 2)));
+    end = inputToTimeT(stoi(end_date.substr(3, 2)) - 1, stoi(end_date.substr(0, 2)), 23, 59, 59);
+
+
+    cout << "Please check input format and try again" << endl; 
+
     
-    time_t start = inputToTimeT(stoi(start_date.substr(3, 2)) - 1, stoi(start_date.substr(0, 2)));
-    time_t end = inputToTimeT(stoi(end_date.substr(3, 2)) - 1, stoi(end_date.substr(0, 2)));
 
     //Search
     int start_index = binarySearchLower(orders, start);
