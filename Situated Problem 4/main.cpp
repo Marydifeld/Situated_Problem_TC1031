0b982cc9c3bc7e1070f7bb2a2586eb05ce14357a
Marydifeld <a01739084@tec.mx>
2025-11-27 20:25:14 -0600

    Implementes search for shortest path

--- Situated Problem 4/main.cpp
+++ Situated Problem 4/main.cpp
@@ -16,6 +16,12 @@ struct ListNode {
             ListNode(const int& element, float p = -1 , ListNode* n = nullptr)
                 : neighborID(element), weight(p), next(n) { }
         };
+struct HeapNode {
+        int nodeID; 
+        float distance; 
+        HeapNode(const int& index, float d)
+        : nodeID(index), distance(d) { }
+};
 class LinkedList {
     private: 
         ListNode* first = nullptr;
@@ -56,6 +62,72 @@ class LinkedList {
             }
             return nullptr;
         }			
+
+};
+class MinHeap {
+    public: 
+        vector<HeapNode*> H; 
+        int capacity; 
+        int size;
+        
+        MinHeap(int cap){
+            capacity = cap;
+            H.resize(cap); 
+            size = 0; 
+        }
+
+        int parent(int i) {return (i - 1)/2;}
+        int leftChild(int i) {return (2*i + 1);}
+        int rightChild(int i) {return (2*i + 2);}
+
+        void insert(HeapNode* node){
+            if (size == capacity) return; 
+
+            H[size] = node; 
+            int i = size; 
+            size++; 
+
+            while (i > 0 && H[parent(i)]->distance > H[i]->distance) {
+                swap(H[parent(i)], H[i]); 
+                i = parent(i);
+            }
+
+        }
+
+        void percolateDown(int i) {
+            int leftCh = leftChild(i); 
+            int rightCh = rightChild(i); 
+            int smallest = i;
+            
+            
+            if (leftCh < size && H[leftCh]->distance < H[smallest]->distance) {
+                smallest = leftCh; 
+            }
+
+            if (rightCh < size && H[rightCh]->distance < H[smallest]->distance){
+                smallest = rightCh; 
+            }
+
+            if (smallest != i) {
+                swap(H[i], H[smallest]); 
+                percolateDown(smallest); 
+            }
+
+
+        }
+
+        HeapNode* extractMin() {
+            HeapNode* root = H[0];
+            H[0] = H[size - 1];
+            size--;
+            percolateDown(0);
+            return root;
+        }
+
+        bool isEmpty() {
+            return size == 0; 
+        }
+
 
 };
 class MenuGraph {
@@ -145,22 +217,21 @@ struct Coordinate{
             int x; 
             int y; 
             string type; 
-            Coordinate(int a, int b, string t = "House")
-            : x(a), y(b), type(t) {}
+            Coordinate(int a = -1, int b = -1)
+            : x(a), y(b) {}
         };
 class CityGraph {
-    private: 
-        int gridSize; 
     public: 
         
-
+        int gridSize;
         vector<Coordinate> coordinates; 
+        vector<vector<string>> typeOfEstablishment; 
         vector<LinkedList> Adjlocations; 
 
         CityGraph(int gs) {
             gridSize = gs * gs; 
             Adjlocations.resize(gridSize); 
-
+            typeOfEstablishment.resize(gridSize);  
          }
 
         int getLocationID(Coordinate co){
@@ -176,6 +247,23 @@ class CityGraph {
                 if (coordinates[i].x == co.x && coordinates[i].y == co.y) { return i; }
             }
             return -1; 
+        }
+
+        int getRestaurantID(string restaurant) {
+        /**
+         * @brief Finds the index of a specified location in the locations 
+         * matrix. O(n^2) (Why do restaurants have to share a building??)
+         * 
+         * @param name The name of the location to be found
+         * 
+         * @return The index of said location. If not found -1 
+         */
+            for (int i = 0; i < gridSize; i++){
+                for (int j = 0; j < typeOfEstablishment[i].size(); j++)
+                    if (typeOfEstablishment[i][j] == restaurant) { return i; }
+            }
+            return -1; 
+            
         }
         
         void insertLocation(int locationId1, int locationId2, float dist){
@@ -334,7 +422,7 @@ void addRestaurantsToCity(string fileName, CityGraph &g){
     string line; 
     ifstream inFile(fileName);
     if (inFile.is_open()){ 
-        while ( getline(inFile, line) ){
+        while (getline(inFile, line) ){
             istringstream iss(line);
 
             //Temporary variables
@@ -352,25 +440,157 @@ void addRestaurantsToCity(string fileName, CityGraph &g){
             iss2 >> ch >> x >> ch >> y;            
             Coordinate co(x,y);
             int coId = g.getLocationID(co); 
-            g.coordinates[coId].type = name;  
-             
-           
-            
+            g.typeOfEstablishment[coId].push_back(name);
+              
         }
     }
     inFile.close();
 
  }
+vector<int> dijkstra(CityGraph &g, Coordinate source, vector<float> &d){
+/**
+ * @brief Impletenation of Dijkstra's algorithm 
+ * 
+ * @param 
+ */
+    int n = g.gridSize; 
 
+    vector<float> distance(n, INT_MAX);
+    vector<int> previous(n, -1);
+    vector<bool> visited(n, false);
+
+    MinHeap h(n); 
+    distance[g.getLocationID(source)] = 0; 
+
+    for (int i = 0; i < n; i++) { 
+        HeapNode* aux = new HeapNode(i, distance[i]); 
+        h.insert(aux);
+    }
+    while (!h.isEmpty()){
+       
+        HeapNode* node = h.extractMin();
+        int u = node->nodeID;
+
+        if (visited[u] == true) { continue; } 
+        visited[u] = true; 
+
+        ListNode* cur = g.Adjlocations[u].getFirst();
+
+        while (cur != nullptr) {
+            int v = cur->neighborID; 
+            float cost = cur->weight;
+            
+            if (!visited[v] && distance[u] + cost < distance[v]) {
+                distance[v] = distance[u] + cost;
+                previous[v] = u; 
+                HeapNode* aux = new HeapNode(v, distance[v]); 
+                h.insert(aux);
+                
+
+            }
+            cur = cur->next; 
+        }
+
+    }
+    d = distance;
+    return previous;  
+}
+vector<int> buildPath(int destination, const vector<int>& previous) {
+    vector<int> path;
+    for (int v = destination; v != -1; v = previous[v])
+        path.insert(path.begin(), v);
+
+    return path;
+}
+void printResult(const vector<int>& path, const vector<float>& distance, int goal, CityGraph &g) {
+    int edges = path.size() - 1;
+    float totalDistance = distance[goal];
+
+    cout << "Number of edges: " << edges << endl;
+    cout << "Total distance: " << totalDistance << endl;
+
+    cout << "Route: ";
+    for (int i = 0; i < path.size(); i++) {
+        Coordinate aux = g.coordinates[path[i]];
+        cout << "(" << aux.x << ", " << aux.y << ")";
+        if (i != path.size() - 1)
+            cout << " -> ";
+    }
+    cout << endl;
+}
+void deliverOrder(CityGraph& g, string restaurantName, Coordinate destination) {
+    //Look for Restaurant coordinates
+    Coordinate source = g.coordinates[g.getRestaurantID(restaurantName)]; 
+    int dest = g.getLocationID(destination);
+
+    //Run algorithm 
+    vector<float> distance; 
+    vector<int> previous = dijkstra(g, source, distance);
+    
+    //Build path
+    vector<int> path = buildPath(dest, previous);
+    cout << "Delivering from " << restaurantName << " to " << "( " << destination.x << 
+    ", " << destination.y << " )" << endl; 
+    printResult(path, distance, dest, g); 
+}
+void readOrder(string fileName, CityGraph& g) {
+/**
+ * @brief Takes all relevant information from the orders file. O(n) with n being the number of files. 
+ * 
+ * @param fileName The name of the file 
+ * @param n Refrence to the variable for the restaurant name to be stored
+ * @param s Refrence to the variable for the coordinates to be stored 
+ */
+    string line; 
+    int index; 
+
+    ifstream inFile(fileName);
+
+    if (inFile.is_open()) { 
+        int i = 0;
+        while ( getline(inFile, line) ){
+            //Temporary variables
+            string month, name, order, rest;
+ 
+            char extra; 
+            int day, hour, min, sec; 
+            float price; 
+            istringstream iss(line);
+            //Take order information
+            iss >> month >> day >> hour >> extra >> min >> extra >> sec;  
+            getline(iss, rest);
+            rest.erase(0, 3);
+            size_t pos = rest.find("O:");
+            size_t pos2 = rest.find("(");
+            size_t pos3 = rest.find(")");
+            name = rest.substr(0, pos - 1);
+            order = rest.substr(pos + 2, pos2 - (pos + 2));
+            price = strtof(rest.substr(pos2 + 1, pos3 - (pos2 + 1)).c_str(), nullptr);
+             
+            //Take coordinates
+            
+            istringstream iss2(rest.substr(pos3 + 1)); 
+            int x,y; 
+            char ch; 
+            iss2 >> ch >> x >> ch >> y;            
+            Coordinate s(x,y);
+
+            //Look for path 
+            deliverOrder(g, name, s);
+            
+        }       
+    }
+}
 int main(){
     //MenuGraph g = CreateMenuGraph("menus.txt");
     //g.showMenu("Ana");
     //g.searchDish("Sopa de Tomate"); 
     CityGraph g2 = CreateCityGraph("city30x30.txt", 30);
     addRestaurantsToCity("restaPlaces.txt", g2); 
-    for (int i = 0; i < 100; i++){
-        cout << g2.coordinates[i].type << endl;
-    }
+    readOrder("orders-city30x30.txt", g2); 
+
+
+    
 
     return 0; 
 }
